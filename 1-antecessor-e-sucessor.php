d62770ee1ece1857d211e465d3769d1ec856fe21
Renato Françoso <renato.francoso@infracommerce.com.br>
2023-06-28 08:07:26 -0300

    Criando funções e separando em arquivos

--- 1-antecessor-e-sucessor.php
+++ 1-antecessor-e-sucessor.php
@@ -1,9 +1,10 @@
 <?php
-//Informe um número e ele mostrará o número, o antecessor e o sucessor!
-$stdin = fopen("php://stdin", "r");//standard input
+include "libs/funcoes-terminal.php";
 
-echo "Digite um Número: \n";
-$numero = fgets($stdin);
+//Informe um número e ele mostrará o número, o antecessor e o sucessor!
+$fin = fopen("php://stdin", "r");//standard input
+
+$numero = ler_input("Digite um número: \n");
 echo "O número digitado foi: $numero\n";
 $numero = rtrim($numero, "\n\r");//tira o caracter do ENTER
 echo "Seu sucessor é ".$numero + 1; 
