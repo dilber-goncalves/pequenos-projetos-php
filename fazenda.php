bb93ca5eefbdcab6126b57245af7e24e5b1b4252
Dilber Goncalves <dilberkeybd@gmail.com>
2023-10-03 10:30:20 -0300

    Fazendo Fazenda POO

--- fazenda.php
+++ fazenda.php
@@ -1,5 +1,9 @@
 <?php
 
+include 'class_Vaca.php';
+include 'class_Cavalo.php';
+include 'class_Ovelha.php';
+
 class Terminal{
     public function write($texto){
         echo $texto;
@@ -8,38 +12,5 @@ class Terminal{
         echo $texto."\n";
     }
 }
-class Vaca {
-    private $cor="malhada"; 
-    public $nome;
-    public function getCor(){
-        return $this->cor;
-    }
-    public function setCor($cor){
-        $coresPermitidas = array("amarela", "malhada", "preta", "branca", "marrom");
-        if (!in_array($cor, $coresPermitidas)){
-            echo "Cor inválida: $cor\n";
-            return;   
-        }
-        $this->cor=$cor;
-    }
-    public function imprimeVaca(){
-        $terminal = new Terminal();
-        $terminal->writeln("Vaca: $this->nome, cor: $this->cor");
-    }
-}
 
-$mimosa = new Vaca();
-$mimosa->setCor("preta");
-$mimosa->nome="Mimosa";
-
-$bernadete = new Vaca();
-$bernadete->nome="Bernadete";
-
-$adelaide = new Vaca();
-$adelaide->setCor("marrom");
-$adelaide->nome="Adelide";
-
-$bernadete->imprimeVaca();
-$mimosa->imprimeVaca();
-$adelaide->imprimeVaca();
 ?>
